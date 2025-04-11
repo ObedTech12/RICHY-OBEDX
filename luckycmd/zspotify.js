f088cdec0d7d4582d6f22abd641d901b6be54429
Obed tech <obedomoke18@gmail.com>
2025-04-11 11:53:48 +0300

    Update zspotify.js

--- luckycmd/zspotify.js
+++ luckycmd/zspotify.js
@@ -25,7 +25,7 @@ ezra({
     }
 
     // Construct playlist message
-    let playlistMessage = `LUCKY PLANET SPOTIFY PLAY\n\n`;
+    let playlistMessage = `RICHY PLANET SPOTIFY PLAY\n\n`;
 
     // Loop through search results and construct track info with numbers
     searchData.forEach((track, index) => {
@@ -46,8 +46,8 @@ ezra({
           mentionedJid: [dest],  // Mention the sender's JID
           externalAdReply: {
             showAdAttribution: true,
-            title: "LUCKY MD SPOTIFY PLAY",
-            body: "Credit by frediezra",
+            title: "RICHY SPOTIFY PLAY",
+            body: "Credit by ObedTechX",
             sourceUrl: "https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f",
             mediaType: 1,
             renderLargerThumbnail: false,
