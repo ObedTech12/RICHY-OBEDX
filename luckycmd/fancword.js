dfceadd321cdfab85315af86b11768441fa66cbc
Obed tech <obedomoke18@gmail.com>
2025-04-11 11:27:46 +0300

    Update fancword.js

--- luckycmd/fancword.js
+++ luckycmd/fancword.js
@@ -8,7 +8,7 @@ ezra({ nomCom: "fancy", categorie: "Fun", reaction: "✍️" }, async (dest, zk,
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 LUCKY MD 2025\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('LUCKY MD 2025', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 LUCKY MD 2025\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('RICHY OBEDX 2025', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
