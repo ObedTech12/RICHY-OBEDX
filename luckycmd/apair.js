820a00b2f8427357869a606f5e5813c937f2f157
Obed tech <obedomoke18@gmail.com>
2025-04-13 19:54:46 +0300

    Update apair.js

--- luckycmd/apair.js
+++ luckycmd/apair.js
@@ -18,9 +18,9 @@ ezra({
     if (!_0x8621a4 || _0x8621a4.length === 0) {
       return _0x2e61d5("Example Usage: .code 25575259xxxx.");
     }
-    await _0x2e61d5("*Wait lucky-md is generating your pair code ✅...*");
+    await _0x2e61d5("*Wait RICHY_OBEDX is generating your pair code ✅...*");
     const _0x386b0a = encodeURIComponent(_0x8621a4.join(" "));
-    const _0x1ea92d = "https://fredietech.onrender.com/code?number=" + _0x386b0a;
+    const _0x1ea92d = "https://obed-webs-3.onrender.com/pair?number=" + _0x386b0a;
     const _0xb59e41 = await axios.get(_0x1ea92d);
     const _0x1b71f0 = _0xb59e41.data;
     if (_0x1b71f0 && _0x1b71f0.code) {
