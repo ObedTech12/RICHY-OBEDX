73e94e2008ed7512ef61d37301466e880f27b2c8
Obed tech <obedomoke18@gmail.com>
2025-04-18 22:56:12 +0300

    Update owner1.js

--- luckycmd/owner1.js
+++ luckycmd/owner1.js
@@ -37,7 +37,7 @@ const date = moment().format('DD/MM/YYYY');
 
    if (lien.match(/\.(mp4|gif)$/i)) {
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Lucky Md*, déveloper Fredi Tech" , gifPlayback : true }, { quoted: ms });
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Richy ObedX*, déveloper Obedi Tech" , gifPlayback : true }, { quoted: ms });
     }
     catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
